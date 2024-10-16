fd42bedf18be2ebcebbb32578a52c359d36070de
Ece <ecekilicpersonal@gmail.com>
2024-10-16 18:07:38 +0100

    edited commit

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const prompt = require('prompt-sync')();
-const Customer = require('./Customer'); // Ensure this is loaded only once
+const Customer = require('./Customer');
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected!'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-// Welcome message
 console.log("Welcome to the CRM!");
 
-// Function to display the menu
 function showMenu() {
   console.log(`
   What would you like to do?
@@ -24,7 +21,6 @@ function showMenu() {
   `);
 }
 
-// Main function for interacting with the CRM
 async function main() {
   let running = true;
 
@@ -34,7 +30,6 @@ async function main() {
 
     switch (choice) {
       case '1':
-        // Create customer
         const name = prompt('Enter customer name: ');
         const age = prompt('Enter customer age: ');
         const newCustomer = new Customer({ name, age });
@@ -43,7 +38,6 @@ async function main() {
         break;
 
       case '2':
-        // View all customers
         const customers = await Customer.find();
         console.log('Customers:');
         customers.forEach(c => {
@@ -52,7 +46,6 @@ async function main() {
         break;
 
       case '3':
-        // Update a customer
         const updateId = prompt('Enter the customer ID to update: ');
         const newName = prompt('Enter new customer name: ');
         const newAge = prompt('Enter new customer age: ');
@@ -61,14 +54,12 @@ async function main() {
         break;
 
       case '4':
-        // Delete a customer
         const deleteId = prompt('Enter the customer ID to delete: ');
         await Customer.findByIdAndDelete(deleteId);
         console.log('Customer deleted successfully!');
         break;
 
       case '5':
-        // Quit the application
         console.log('Exiting...');
         running = false;
         mongoose.connection.close();
--- customer.js
+++ customer.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// Define the customer schema
 const customerSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -12,7 +11,6 @@ const customerSchema = new mongoose.Schema({
   }
 });
 
-// Create and export the model
 const Customer = mongoose.model('Customer', customerSchema);
 
 module.exports = Customer;
